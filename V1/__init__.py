cf4fc2cbf9a81b4c29c81a8fc8c5a2c2c093c9fd
Mutuba <dmutuba@gmail.com>
2018-06-26 13:03:25 +0300

    Updted config file to include OOP

--- V1/__init__.py
+++ V1/__init__.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
 
 # set config
-app.config.from_object('config')
+app.config.from_object('config.BaseConfig')
 
 
 from V1.auth.views import auth_blueprint
